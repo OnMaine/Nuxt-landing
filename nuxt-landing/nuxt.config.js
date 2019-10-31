7ededbcacb66de95094a3920bc730afcb4a6233a
OnMaine <Onmaine@gmail.com>
2019-11-01 01:31:20 +0200

    Сonnected necessary libraries

--- nuxt-landing/nuxt.config.js
+++ nuxt-landing/nuxt.config.js
@@ -11,6 +11,18 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' }
+    ],
+    script: [
+      { src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js' }
+    ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' }
+    ],
+    script: [
+      { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js' }
     ]
   },
   /*
@@ -36,4 +48,3 @@ module.exports = {
     }
   }
 }
-
